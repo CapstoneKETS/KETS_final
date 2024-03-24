f4b715b7362c97d118f1b969ae0fcef093ee6a8d
정영희 <69373688+jkhyjkhy@users.noreply.github.com>
2024-03-24 19:35:35 +0900

    어드민 페이지 테이블 추가

--- mysite/mysite/mainpage/admin.py
+++ mysite/mysite/mainpage/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import kwRank, kwHistory, newsData
+
+admin.site.register(kwRank)
+admin.site.register(kwHistory)
+admin.site.register(newsData)
 # Register your models here.
