c9f3481fab56eb5b61525224f745ecf75327d127
geonparkpark <150771369+geonparkpark@users.noreply.github.com>
2024-04-01 02:29:48 +0900

    데모 0 버전 통합 (일부 기능 제외)

--- mysite/mysite/mainpage/url.py
+++ mysite/mysite/mainpage/url.py
@@ -5,6 +5,6 @@ app_name = 'mainpage'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('newslist/', views.newsList, name='news_list'),  # /news/ URL에 대한 newsList 뷰 매핑
-    path('details/', views.details, name='details'),  # /details/ URL에 대한 details 뷰 매핑
+    path('<str:kwRank_keyword>/', views.newsList, name='newsList'),  # /news/ URL에 대한 newsList 뷰 매핑
+    path('<str:kwRank_keyword>/<int:newsData_id>/', views.details, name='details'),  # /details/ URL에 대한 details 뷰 매핑
 ]
