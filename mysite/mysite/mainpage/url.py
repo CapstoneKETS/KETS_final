19c796f0fd7cde8c594f09cbfa7e054ace056ff3
geonparkpark <150771369+geonparkpark@users.noreply.github.com>
2024-05-06 17:41:10 +0900

    url패턴 정리

--- mysite/mysite/mainpage/url.py
+++ mysite/mysite/mainpage/url.py
@@ -5,7 +5,6 @@ app_name = 'mainpage'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('?time=<int:datetime>', views.index, name='kwList'),
     path('<str:kwRank_keyword>/', views.newsList, name='newsList'),  # /news/ URL에 대한 newsList 뷰 매핑
     path('<str:kwRank_keyword>/<int:newsData_id>/', views.details, name='details'),  # /details/ URL에 대한 details 뷰 매핑
 ]
